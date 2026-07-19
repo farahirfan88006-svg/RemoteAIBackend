9b5e9bbba3d57d831e74514bc89db6023e44cf1e
Farah <i3565503@gmail.com>
2026-07-19 21:22:13 +0500

    Fix database config exports

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import { env } from "./env.js";
 
 export async function connectDatabase() {
   try {
-    await mongoose.connect(env.MONGO_URI);
+    await mongoose.connect(env.mongodbUri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
